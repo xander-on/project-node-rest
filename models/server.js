b3fc2e4cec614dee68f1ae4bcc21dacf6b4dabaa
Xander <alexandermoreno.dev@gmail.com>
2023-07-26 23:41:08 -0500

    .env.example

--- models/server.js
+++ models/server.js
@@ -5,7 +5,7 @@ class Server{
 
     constructor(){
         this.app       = express();
-        this.port      = process.env.PORT;
+        this.port      = process.env.PORT || 3000;
         this.usersPath = '/api-users/v1/users';
 
         //Middlewares
